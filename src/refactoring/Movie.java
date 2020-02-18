009571a724ab16d781acd736ce010198dd8c8910
eiji.okuda <eiji.okuda@matera.com>
2020-02-18 09:21:15 -0300

    replace conditional with polimorphism getCharge

--- src/refactoring/Movie.java
+++ src/refactoring/Movie.java
@@ -4,48 +4,83 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
+    private Price price;
     private String _title;
-    private int _priceCode;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
     public int getPriceCode() {
-        return _priceCode;
+        return price.getPriceCode();
     }
-    public void setPriceCode(int arg) {
-        _priceCode = arg;
+    public void setPriceCode(int princeCode) {
+        if (princeCode == REGULAR) {
+            price = new RegularPrice();
+        } else if (princeCode == NEW_RELEASE) {
+            price = new NewReleasePrice();
+        } else if (princeCode == CHILDRENS) {
+            price = new ChildrenPrice();
+        } else {
+            throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
     public String getTitle (){
         return _title;
     };
 
     double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+        return price.getCharge(daysRented);
     }
 
     int getFrequentRenterPoints(int daysRented) {
         int frequentRenterPoints = 1;
         // add bonus for a two day new release rental
-        if ((getPriceCode() == NEW_RELEASE) && daysRented > 1)
+        if ((price.getPriceCode() == NEW_RELEASE) && daysRented > 1)
             frequentRenterPoints++;
         return frequentRenterPoints;
     }
+
+    private abstract class Price {
+
+        public abstract int getPriceCode() ;
+
+        protected abstract double getCharge(int daysRented) ;
+    }
+
+    public class ChildrenPrice extends Price {
+        @Override
+        public int getPriceCode() {
+            return Movie.CHILDRENS;
+        }
+        protected double getCharge(int daysRented) {
+            double result = 1.5;
+            if (daysRented > 3)
+                result += (daysRented - 3) * 1.5;
+            return result;
+        }
+    }
+    public class NewReleasePrice extends Price {
+        @Override
+        public int getPriceCode() {
+            return Movie.NEW_RELEASE;
+        }
+        protected double getCharge(int daysRented) {
+            return daysRented * 3;
+        }
+    }
+    public class RegularPrice extends Price {
+        @Override
+        public int getPriceCode() {
+            return Movie.REGULAR;
+        }
+        protected double getCharge(int daysRented) {
+            double result = 2;
+            if (daysRented > 2)
+                result += (daysRented - 2) * 1.5;
+
+            return result;
+        }
+    }
+
 }
