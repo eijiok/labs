e43567e3f3ad386edbbd438f40f0249256de2d9b
eiji.okuda <eiji.okuda@matera.com>
2020-02-18 09:28:07 -0300

    replace conditional with polimorphism getFrequentRenterPoints

--- src/refactoring/Movie.java
+++ src/refactoring/Movie.java
@@ -1,5 +1,10 @@
 package refactoring;
 
+import refactoring.price.ChildrenPrice;
+import refactoring.price.NewReleasePrice;
+import refactoring.price.Price;
+import refactoring.price.RegularPrice;
+
 public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
@@ -34,53 +39,7 @@ public class Movie {
     }
 
     int getFrequentRenterPoints(int daysRented) {
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
-        if ((price.getPriceCode() == NEW_RELEASE) && daysRented > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
-    }
-
-    private abstract class Price {
-
-        public abstract int getPriceCode() ;
-
-        protected abstract double getCharge(int daysRented) ;
-    }
-
-    public class ChildrenPrice extends Price {
-        @Override
-        public int getPriceCode() {
-            return Movie.CHILDRENS;
-        }
-        protected double getCharge(int daysRented) {
-            double result = 1.5;
-            if (daysRented > 3)
-                result += (daysRented - 3) * 1.5;
-            return result;
-        }
-    }
-    public class NewReleasePrice extends Price {
-        @Override
-        public int getPriceCode() {
-            return Movie.NEW_RELEASE;
-        }
-        protected double getCharge(int daysRented) {
-            return daysRented * 3;
-        }
-    }
-    public class RegularPrice extends Price {
-        @Override
-        public int getPriceCode() {
-            return Movie.REGULAR;
-        }
-        protected double getCharge(int daysRented) {
-            double result = 2;
-            if (daysRented > 2)
-                result += (daysRented - 2) * 1.5;
-
-            return result;
-        }
+        return price.getFrequentRenterPoints(daysRented);
     }
 
 }
---
+++ src/refactoring/price/RegularPrice.java
@@ -0,0 +1,17 @@
+package refactoring.price;
+
+import refactoring.Movie;
+
+public class RegularPrice extends Price {
+    @Override
+    public int getPriceCode() {
+        return Movie.REGULAR;
+    }
+    public double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+
+        return result;
+    }
+}
