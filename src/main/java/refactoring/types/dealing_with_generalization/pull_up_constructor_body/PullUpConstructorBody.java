23ae5201a1de2da19add230a4eb7ad4eec18780d
eiji.okuda <eiji.okuda@matera.com>
2020-03-11 07:52:22 -0300

    PullUpConstructorBody

---
+++ src/main/java/refactoring/types/dealing_with_generalization/pull_up_constructor_body/PullUpConstructorBody.java
@@ -0,0 +1,11 @@
+package refactoring.types.dealing_with_generalization.pull_up_constructor_body;
+
+/**
+ * <h1>Pull Up Constructor Body</h1>
+ * <p><strong>Problem</strong>: Your subclasses have constructors with code that’s mostly identical.</p>
+ * <p><strong>Solution</strong>: Create a superclass constructor and move the code that’s the same in the subclasses to it. Call the superclass constructor in the subclass constructors.</p>
+ * <br />
+ * <p><strong>Task</strong>: </p>
+ */
+public class PullUpConstructorBody {
+}
